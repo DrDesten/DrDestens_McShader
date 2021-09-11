57b612a7b20359f98e35068dcada11ef57ecf65c
DrDesten <70536771+DrDesten@users.noreply.github.com>
2021-09-11 20:54:04 +0200

    Replacing Everything due to fundamental Restructuring

--- shaders/lib/world_curve.glsl
+++
@@ -1,7 +0,0 @@
-vec4 unmodifiedClipPosition   = ftransform();
-vec4 modifiedPlayerPosition   = gbufferModelViewInverse * (gl_ModelViewMatrix * gl_Vertex);
-float dist2                   = dot(modifiedPlayerPosition.xz, modifiedPlayerPosition.xz);
-float worldCurvatureRadius    = WORLD_CURVE_RADIUS + modifiedPlayerPosition.y + cameraPosition.y;
-modifiedPlayerPosition.y     += sqrt( (worldCurvatureRadius * worldCurvatureRadius) - dist2 ) - worldCurvatureRadius; // Faster version, doesn't work with negative radii
-//modifiedPlayerPosition.y     += worldCurvatureRadius * sqrt(1 - (dist2 / (worldCurvatureRadius * worldCurvatureRadius))) - worldCurvatureRadius;
-clipPos 	                 += gl_ProjectionMatrix * (gbufferModelView * modifiedPlayerPosition) - unmodifiedClipPosition;
