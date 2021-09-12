5079b2eaae48558e702f4c2f48b6826bb3750374
DrDesten <70536771+DrDesten@users.noreply.github.com>
2021-09-12 21:21:46 +0200

    Restructured Menu

--- shaders/lib/math.glsl
+++ shaders/lib/math.glsl
@@ -65,34 +65,6 @@ float smoothCutoff(float x, float cutoff, float taper) {
     return clamp( (a * tmp) * (tmp * tmp) + x ,0,1);
 }
 
-
-////////////////////////////////////////////////////////////////////////
-// Color-Specific functions
-
-vec3 saturation(vec3 col, float saturation) {
-    float brightness = dot(col, vec3(0.299, 0.587, 0.112));
-    return mix(vec3(brightness), col, saturation);
-}
-
-float luminance(vec3 color) {
-    return dot(color, vec3(0.2126, 0.7152, 0.0722));
-}
-
-vec3 rgb2hsv(vec3 c) {
-    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-
-    float d = q.x - min(q.w, q.y);
-    float e = 1.0e-10;
-    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
-}
-vec3 hsv2rgb(vec3 c) {
-    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
-
 ////////////////////////////////////////////////////////////////////////
 // Randomization and Dither Patterns
 
@@ -322,6 +294,40 @@ mat3 arbitraryTBN(vec3 normal) {
     // Equivalent to: cross(normal, tangent)
     vec3 binomial = vec3(-normal.x * normal.z, normal.x * normal.z, (normal.y * normal.y) + (normal.x * normal.x));
     return mat3(normalize(tangent), normalize(binomial), normal);
+}
+
+
+////////////////////////////////////////////////////////////////////////
+// Color-Specific functions
+
+vec3 saturation(vec3 col, float saturation) {
+    float brightness = dot(col, vec3(0.299, 0.587, 0.112));
+    return mix(vec3(brightness), col, saturation);
+}
+
+vec3 contrast(vec3 col, float contrast) {
+    vec3 lower = (contrast * col) * (col * col);
+    vec3 upper = 1 - contrast * sq(col - 1);
+    return mix(lower, upper, col);
+}
+
+float luminance(vec3 color) {
+    return dot(color, vec3(0.2126, 0.7152, 0.0722));
+}
+
+vec3 rgb2hsv(vec3 c) {
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 }
 
 /////////////////////////////////////////////////////////////////////////////////
