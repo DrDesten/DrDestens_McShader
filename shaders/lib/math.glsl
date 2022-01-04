09fa972a541d7cd59c543263a1d67f290bcc90de
DrDesten <>
2022-01-04 20:29:18 +0100

    Added some functions

--- shaders/lib/math.glsl
+++ shaders/lib/math.glsl
@@ -447,6 +447,28 @@ float angle(vec2 v) {
 ////////////////////////////////////////////////////////////////////////
 // (Un)Packing Functions
 
+vec2 signNotZero(vec2 v) {
+    return vec2((v.x >= 0.0) ? +1.0 : -1.0, (v.y >= 0.0) ? +1.0 : -1.0);
+}
+
+vec2 octahedralEncode(in vec3 v) {
+    float l1norm = abs(v.x) + abs(v.y) + abs(v.z);
+    vec2  result = v.xy * (1.0 / l1norm);
+    if (v.z < 0.0) {
+        result = (1.0 - abs(result.yx)) * signNotZero(result.xy);
+    }
+    return result;
+}
+
+vec3 octahedralDecode(vec2 o) {
+    vec3 v = vec3(o.x, o.y, 1.0 - abs(o.x) - abs(o.y));
+    if (v.z < 0.0) {
+        v.xy = (1.0 - abs(v.yx)) * signNotZero(v.xy);
+    }
+    return normalize(v);
+}
+
+
 vec3 Int24ToVec3_asfloat(float x) {
     int ix = int(x * 16777216.);
     return vec3(
